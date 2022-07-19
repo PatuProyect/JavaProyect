4c9a54fe886f75c887cdae566e0336d154aa8c26
Diego Bastida <Diego.Bastida@fravega.com.ar>
2022-07-19 14:37:57 -0300

    Ejercicio tema 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        var resultant = suma(23,56,76);
+        System.out.println(resultant);
+
+        Coche miCoche = new Coche();
+        miCoche.SummaryPuerto();
+        System.out.println(miCoche.puerta);
+    }
+
+    public static int suma (int a,int b,int c){
+        return a + b + c;
+    }
+
+
+}
+
+ class Coche {
+    public int puerta = 4;
+
+    public void SummaryPuerto() {
+        this.puerta++;
     }
 }
