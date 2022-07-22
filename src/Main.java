10cc3b8e29ed530afe649b804171f8a6c4c27907
Diego Bastida <diegoabastida@gmail.com>
2022-07-22 12:04:26 -0300

    ejercicios tema 4

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,48 @@
 public class Main {
     public static void main(String[] args) {
+        int numeroIf = 3;
+        if (numeroIf == 0){
+            System.out.println("Es igual a 0");
+        } else if (numeroIf < 0) {
+            System.out.println("Es negativo");
+        } else {
+            System.out.println("Es positivo");
+        }
 
-        var resultant = suma(23,56,76);
-        System.out.println(resultant);
+        int numeroWhile = 0;
+        while (numeroWhile < 3){
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }
 
-        Coche miCoche = new Coche();
-        miCoche.SummaryPuerto();
-        System.out.println(miCoche.puerta);
-    }
+        int numeroDo = 0;
+        do {
+            numeroDo++;
+        }while (numeroDo < 3) ;{
+            System.out.println(numeroDo);
+        }
 
-    public static int suma (int a,int b,int c){
-        return a + b + c;
+        for (int i = 0; i <=3; i++){
+            System.out.println(i);
+        }
+
+        var estacion = "verano";
+        switch (estacion){
+            case "verano":
+                System.out.println("es verano");
+                break;
+            case "otonio":
+                System.out.println("es otonio");
+                break;
+            case "invierno":
+                System.out.println("es invierno");
+                break;
+            case "primavera":
+                System.out.println("es primavera");
+                break;
+            default:
+                System.out.println("no hay una estacion definida");
+        }
     }
 }
 
- class Coche {
-    public int puerta = 4;
-
-    public void SummaryPuerto() {
-        this.puerta++;
-    }
-}
