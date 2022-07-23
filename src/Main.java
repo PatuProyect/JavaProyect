96b5fc73a2f469a07dbab793123f182dcd2de1d9
Diego Bastida <diegoabastida@gmail.com>
2022-07-23 10:16:48 -0300

    ejercicios tema 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+
+        var resultado = suma(10,30,40);
+        System.out.println(resultado);
+
+        Coche MiCoche = new Coche();
+        MiCoche.SumarPuerta();
+        MiCoche.SumarPuerta();
+        System.out.println(MiCoche.puerta);
+
+
+
+
         int numeroIf = 3;
         if (numeroIf == 0){
             System.out.println("Es igual a 0");
@@ -43,6 +55,17 @@ public class Main {
             default:
                 System.out.println("no hay una estacion definida");
         }
+
+    }
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
     }
 }
 
+class Coche {
+    public int puerta = 1;
+
+    public void SumarPuerta(){
+        this.puerta++;
+    }
+}
